1436f13adfafd55d67144005620d5115f40f3317
box-o-water <boxooowater@gmail.com>
2022-12-03 22:45:33 -0600

    feat(display-data): display 5 data points (temp date times) from the api on the page

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var lon = -93.235190
 var lang = 'en'
 var units = 'imperial'
 
+var listEL = document.getElementById("myData")
+
 function getApi() {
     var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=${units}`;
   
@@ -14,7 +16,19 @@ function getApi() {
         })
         .then(function (data) {
             console.log(data)
-    });
+            var someArray = data.list;
+            for (var i = 0; i < 5; i++) {
+                // var listItem = someArray[i].dt_txt;
+                // console.log(listItem)
+                var listItem = document.createElement("li");
+                listItem.textContent = someArray[i].dt_txt;
+                console.log(listItem)
+                listEL.appendChild(listItem);
+            }
+        })
+        .catch(function (error){
+            console.log(error)
+        })
 };
 
 function init() {
